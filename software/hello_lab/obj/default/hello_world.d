6a866a1a126f6b53b3394a32742135a3508a20a2
Christian Motta <chrismotta2005@gmail.com>
2026-04-02 17:25:04 +1300

    Init commit with lab1 files

---
+++ software/hello_lab/obj/default/hello_world.d
@@ -0,0 +1,14 @@
+obj/default/hello_world.o: hello_world.c ../hello_lab_bsp/system.h \
+ ../hello_lab_bsp/linker.h \
+ ../hello_lab_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../hello_lab_bsp//HAL/inc/io.h ../hello_lab_bsp//HAL/inc/alt_types.h
+
+../hello_lab_bsp/system.h:
+
+../hello_lab_bsp/linker.h:
+
+../hello_lab_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../hello_lab_bsp//HAL/inc/io.h:
+
+../hello_lab_bsp//HAL/inc/alt_types.h:
