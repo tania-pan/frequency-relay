c9b153a487895c5caa42c63ea48c6d62d83023a7
Christian Motta <chrismotta2005@gmail.com>
2026-04-04 17:16:53 +1300

    Renamed proj, declared data structs, wrote init function - builds

---
+++ software/frequency_relay/obj/default/FreeRTOS/list.d
@@ -0,0 +1,37 @@
+obj/default/FreeRTOS/list.o: FreeRTOS/list.c FreeRTOS/FreeRTOS.h \
+ FreeRTOS/FreeRTOSConfig.h ../frequency_relay_bsp/system.h \
+ ../frequency_relay_bsp/linker.h FreeRTOS/projdefs.h FreeRTOS/portable.h \
+ FreeRTOS/deprecated_definitions.h FreeRTOS/portmacro.h \
+ ../frequency_relay_bsp//HAL/inc/sys/alt_irq.h \
+ ../frequency_relay_bsp//HAL/inc/nios2.h \
+ ../frequency_relay_bsp//HAL/inc/alt_types.h \
+ ../frequency_relay_bsp//HAL/inc/priv/alt_legacy_irq.h \
+ FreeRTOS/mpu_wrappers.h FreeRTOS/list.h
+
+FreeRTOS/FreeRTOS.h:
+
+FreeRTOS/FreeRTOSConfig.h:
+
+../frequency_relay_bsp/system.h:
+
+../frequency_relay_bsp/linker.h:
+
+FreeRTOS/projdefs.h:
+
+FreeRTOS/portable.h:
+
+FreeRTOS/deprecated_definitions.h:
+
+FreeRTOS/portmacro.h:
+
+../frequency_relay_bsp//HAL/inc/sys/alt_irq.h:
+
+../frequency_relay_bsp//HAL/inc/nios2.h:
+
+../frequency_relay_bsp//HAL/inc/alt_types.h:
+
+../frequency_relay_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+FreeRTOS/mpu_wrappers.h:
+
+FreeRTOS/list.h:
---
+++ software/frequency_relay/obj/default/frequency_relay.d
@@ -0,0 +1,57 @@
+obj/default/frequency_relay.o: frequency_relay.c freertos/FreeRTOS.h \
+ freertos/FreeRTOSConfig.h ../frequency_relay_bsp/system.h \
+ ../frequency_relay_bsp/linker.h freertos/projdefs.h freertos/portable.h \
+ freertos/deprecated_definitions.h freertos/portmacro.h \
+ ../frequency_relay_bsp//HAL/inc/sys/alt_irq.h \
+ ../frequency_relay_bsp//HAL/inc/nios2.h \
+ ../frequency_relay_bsp//HAL/inc/alt_types.h \
+ ../frequency_relay_bsp//HAL/inc/priv/alt_legacy_irq.h \
+ freertos/mpu_wrappers.h freertos/task.h freertos/list.h freertos/queue.h \
+ freertos/semphr.h freertos/queue.h \
+ ../frequency_relay_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../frequency_relay_bsp//HAL/inc/io.h \
+ ../frequency_relay_bsp//HAL/inc/alt_types.h frequency_relay.h
+
+freertos/FreeRTOS.h:
+
+freertos/FreeRTOSConfig.h:
+
+../frequency_relay_bsp/system.h:
+
+../frequency_relay_bsp/linker.h:
+
+freertos/projdefs.h:
+
+freertos/portable.h:
+
+freertos/deprecated_definitions.h:
+
+freertos/portmacro.h:
+
+../frequency_relay_bsp//HAL/inc/sys/alt_irq.h:
+
+../frequency_relay_bsp//HAL/inc/nios2.h:
+
+../frequency_relay_bsp//HAL/inc/alt_types.h:
+
+../frequency_relay_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+freertos/mpu_wrappers.h:
+
+freertos/task.h:
+
+freertos/list.h:
+
+freertos/queue.h:
+
+freertos/semphr.h:
+
+freertos/queue.h:
+
+../frequency_relay_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../frequency_relay_bsp//HAL/inc/io.h:
+
+../frequency_relay_bsp//HAL/inc/alt_types.h:
+
+frequency_relay.h:
