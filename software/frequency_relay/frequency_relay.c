d2f92db58afc7ffd40f17f87d372dd3504309750
chrismotta2005@gmail.com <chrismotta2005@gmail.com>
2026-04-12 00:20:56 +1200

    Consumer task to test ISRs

--- software/frequency_relay/frequency_relay.c
+++ software/frequency_relay/frequency_relay.c
@@ -5,6 +5,7 @@
 
 // scheduler includes
 #include "freertos/FreeRTOS.h"
+#include "freertos/projdefs.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
 #include "freertos/semphr.h"
@@ -29,6 +30,32 @@ SemaphoreHandle_t systemStatusMutex;
 SemaphoreHandle_t timingLogMutex;
 
 freqData_t freq_data;
+
+void debug_consumer_task(void *pvParameters) {
+	// TODO: remove once tested and working correctly
+	int button_rx;
+	int kbd_rx;
+
+	printf("Debug task: waiting for ISR triggers");
+
+	while(1) {
+		// check semaphore if new resource has appeared
+		if (xSemaphoreTake(peakReadSem, 0)) {
+			printf("FAU semaphore accessed");
+		}
+		// check button queue
+		if (xQueueReceive(buttonCmdQ, &button_rx, 0) == pdTRUE) {
+			printf("Button queue accessed: Value = %u", button_rx);
+		}
+		// check kbd queue
+		if (xQueueReceive(kbdQ, &kbd_rx, 0) == pdTRUE) {
+			printf("Keyboard queue accessed: Value = %u", kbd_rx);
+		}
+	}
+
+	// yield to scheduler for 50ms 
+	vTaskDelay(pdMS_TO_TICKS(50));
+}
 
 void init_config(void) {
 	// init global data
