ebf7ce897d012994b035671a91330b4b52065627
Christian Motta <chrismotta2005@gmail.com>
2026-04-19 16:54:07 +1200

    Render system state, loads, and timing, and uptime

--- software/frequency_relay/vga.c
+++ software/frequency_relay/vga.c
@@ -12,7 +12,7 @@
 void vga_display_task(void *pvParameters) {
     // grab char buffer device handle from pv params
     alt_up_char_buffer_dev *char_buffer = (alt_up_char_buffer_dev *)pvParameters;
-
+    
     // ~60Hz (17ms) timing setup
     TickType_t last_wake_ticks = xTaskGetTickCount();
     const TickType_t freq_ticks = pdMS_TO_TICKS(17);
@@ -75,44 +75,92 @@ void vga_display_task(void *pvParameters) {
 
         // -- gather local copies of data for display --
         // peak (don't steal from load management)
-            if (xQueuePeek(freq_data_q, &local_freq_data, 0) != pdTRUE) {
-                local_freq_data.frequency = 0;
-                local_freq_data.roc = 0;
-            }
+        if (xQueuePeek(freq_data_q, &local_freq_data, 0) != pdTRUE) {
+            local_freq_data.frequency = 0;
+            local_freq_data.roc = 0;
+        }
 
         // lock then grab copy of system status to display
-            if (xSemaphoreTake(system_status_mutex, portMAX_DELAY)) {
-                local_system_status = system_status;
-                xSemaphoreGive(system_status_mutex);
-            }
-            if (xSemaphoreTake(load_status_mutex, portMAX_DELAY)) {
+        if (xSemaphoreTake(system_status_mutex, portMAX_DELAY)) {
+            local_system_status = system_status;
+            xSemaphoreGive(system_status_mutex);
+        }
+        if (xSemaphoreTake(load_status_mutex, portMAX_DELAY)) {
             for (int i = 0; i < NUM_LOADS; i++) {
                 local_load_status[i] = load_status[i];
             }
-                xSemaphoreGive(load_status_mutex);
-            }
-            if (xSemaphoreTake(timing_log_mutex, portMAX_DELAY)) {
+            xSemaphoreGive(load_status_mutex);
+        }
+        if (xSemaphoreTake(timing_log_mutex, portMAX_DELAY)) {
             local_timing_log = timing_log;
-                xSemaphoreGive(timing_log_mutex);
-            }
+            xSemaphoreGive(timing_log_mutex);
+        }
 
         // -- render VGA elements (current measurements) --
-            sprintf(text_buffer, "[%d] TF Threshold: %5.2f Hz    ", 
+        sprintf(text_buffer, "[%d] TF Threshold: %5.2f Hz    ", 
             (1 - local_system_status.threshold_edit_mode), local_system_status.TF_threshold);
-            alt_up_char_buffer_string(char_buffer, text_buffer, 5, 5);
-            
-            sprintf(text_buffer, "[%d] TROC Threshold: %5.2f Hz/s    ", 
+        alt_up_char_buffer_string(char_buffer, text_buffer, 5, 5);
+        
+        sprintf(text_buffer, "[%d] TROC Threshold: %5.2f Hz/s    ", 
             local_system_status.threshold_edit_mode, local_system_status.TROC_threshold);
-            alt_up_char_buffer_string(char_buffer, text_buffer, 5, 7);
-            
-            sprintf(text_buffer, "Current Frequency: %f Hz    ", local_freq_data.frequency);
-            alt_up_char_buffer_string(char_buffer, text_buffer, 5, 9);
+        alt_up_char_buffer_string(char_buffer, text_buffer, 5, 7);
+        
+        sprintf(text_buffer, "Current Frequency: %5.2f Hz    ", local_freq_data.frequency);
+        alt_up_char_buffer_string(char_buffer, text_buffer, 5, 9);
 
         sprintf(text_buffer, "Current RoC: %5.2f Hz/s    ", local_freq_data.roc);
-            alt_up_char_buffer_string(char_buffer, text_buffer, 5, 11);
+        alt_up_char_buffer_string(char_buffer, text_buffer, 5, 11);
 
-            // TODO: add load and timing stats here
-
-            vTaskDelayUntil(&last_wake_ticks, freq_ticks);
+        // -- current mode
+        const char* state_str;
+        switch (local_system_status.system_state) {
+            case SYSTEM_NORMAL:
+                state_str = "NORMAL     ";
+                break;
+            case SYSTEM_MAINTENANCE: 
+                state_str = "MAINTENANCE"; 
+                break;
+            case SYSTEM_MANAGING:    
+                state_str = "MANAGING   "; 
+                break;
+            default:                 
+                state_str = "UNKNOWN    "; 
+                break;
         }
+        sprintf(text_buffer, "System State: %s", state_str);
+        alt_up_char_buffer_string(char_buffer, text_buffer, 5, 13);
+
+        //  -- load statuses --
+        for (int i = 0; i < NUM_LOADS; i++) {
+            const char* load_str;
+            if (local_load_status[i] == LOAD_ON) load_str = "ON  ";
+            else if (local_load_status[i] == LOAD_SHED) load_str = "SHED";
+            else load_str = "OFF ";
+            
+            sprintf(text_buffer, "Load %d: %s", i+1, load_str);
+            // space loads out by 2 char units
+            alt_up_char_buffer_string(char_buffer, text_buffer, 5, 16 + (i*2));
+        }
+
+        // -- timing stats --
+        sprintf(text_buffer, "--- Reaction Times (Ticks) ---");
+        alt_up_char_buffer_string(char_buffer, text_buffer, 45, 5);
+
+        sprintf(text_buffer, "Recent: %d, %d, %d, %d, %d       ", 
+            (int)local_timing_log.recent[0], (int)local_timing_log.recent[1], 
+            (int)local_timing_log.recent[2], (int)local_timing_log.recent[3], 
+            (int)local_timing_log.recent[4]);
+        alt_up_char_buffer_string(char_buffer, text_buffer, 45, 7);
+
+        sprintf(text_buffer, "Min: %d | Max: %d | Avg: %d    ", 
+            (int)local_timing_log.min_time, (int)local_timing_log.max_time, (int)local_timing_log.avg_time);
+        alt_up_char_buffer_string(char_buffer, text_buffer, 45, 9);
+
+        // -- uptime --
+        unsigned int uptime_sec = xTaskGetTickCount() / configTICK_RATE_HZ;
+        sprintf(text_buffer, "System Uptime: %u seconds      ", uptime_sec);
+        alt_up_char_buffer_string(char_buffer, text_buffer, 45, 12);
+
+        vTaskDelayUntil(&last_wake_ticks, freq_ticks);
     }
+}
